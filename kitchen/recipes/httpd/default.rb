3998607dcbdb3b747bb193650076357dc291fbab
kappa <kappa@kappabookair.local>
2015-08-07 17:16:46 +0900

    add ubuntu

--- kitchen/recipes/httpd/default.rb
+++ kitchen/recipes/httpd/default.rb
@@ -1,5 +1,12 @@
-package "httpd"
-
-service "httpd" do
-  action [ :start, :enable ]
+case node[:platform]
+when "ubuntu", "debian"
+  package "apache2"
+  service "apache2" do
+    action [ :start, :enable ]
+  end
+when "centos", "redhat", "amazon"
+  package "httpd"
+  service "httpd" do
+    action [ :start, :enable ]
+  end
 end
